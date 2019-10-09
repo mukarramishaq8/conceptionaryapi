28cae763144ac6643061f5a07311e0dfa46d9be8
Abdul Basit <basit850@gmail.com>
2019-10-09 16:22:43 +0500

    site map fianl

--- sitemap-generator.js
+++ sitemap-generator.js
@@ -1,159 +1,113 @@
 require("babel-register")({
-    presets: ["es2015", "react"]
-  });
-  const Sequelize = require('sequelize');
-
-  const router = require("./sitemap-routes").default;
-  const Sitemap = require("react-router-sitemap").default;
-  const db = require('./app/bootstrap');
-  
+  presets: ["es2015", "react"]
+});
+const Sequelize = require('sequelize');
+const router = require("./sitemap-routes").default;
+const Sitemap = require("react-router-sitemap").default;
+const db = require('./app/bootstrap');
 const Concept = db.Concept;
 const ConceptCluster = db.ConceptCluster;
 const Author = db.Author;
-const Perspective=db.Perspective;
-const AuthorGroups=db.AuthorGroups;
+const Perspective = db.Perspective;
+const AuthorGroups = db.AuthorGroups;
+var domain="http://conceptionary.io"
 
-async function generateSitemap() {
-  let conceptMap=[];
-  let authorMap=[];
-  let conceptClusterMap=[]
-  let perspectiveMap=[]
-  let authorGroupMap=[]
-  await Concept.findAll({attributes: ['name']}).then(response => {
-      console.log("first is ",response[0])
-      response.map(data => conceptMap.push({name:data.name}));
-    })
-    console.log("size is ",conceptMap[0]);
 
-    let idMap = [{name:'love'},{name:'love'},{name:'educate'},{name:'cdk'},{name:'voice'},{name:'song'}];
-    // let authorMap = [{name:'Hao'},{name:'FriedRich'},{name:'Sigmund Frued'}];
-    // conceptMap=result.concepts;
-  
-
-    
-    
-    const paramsConfig = {
-        "/Concept/:name": conceptMap,
-      };
-  
-    
-    
-    return (
-        new Sitemap(router)
-            .applyParams(paramsConfig)
-            .build("http://conceptionary.io")
-            .save("./public/sitemap.xml")
-      );
-  }
-async function generateAuthorSitedMap() {
-  let conceptMap=[];
-  let authorMap=[];
-  let conceptClusterMap=[]
-  let perspectiveMap=[]
-  let authorGroupMap=[]
-    await Author.findAll({attributes: ['firstName','lastName']}).then(response => {
-      console.log("first author is ",JSON.stringify(response[0]))
-      response.map(data => authorMap.push({name:data.firstName+" "+data.lastName}));
-      
-    })
-    
-    let idMap = [{name:'love'},{name:'love'},{name:'educate'},{name:'cdk'},{name:'voice'},{name:'song'}];
-    // let authorMap = [{name:'Hao'},{name:'FriedRich'},{name:'Sigmund Frued'}];
-    // conceptMap=result.concepts;
-  
-
-    
-    
-    const paramsConfig = {
-        "/Author/:name":authorMap,
-      };
-  
-    
-    
-    return (
-        new Sitemap(router)
-            .applyParams(paramsConfig)
-            .build("http://conceptionary.io")
-            .save("./public/authorsitemap.xml")
-      );
-  }
-async function generateConceptClusterSitedMap() {
-  let conceptMap=[];
-  let authorMap=[];
-  let conceptClusterMap=[]
-  let perspectiveMap=[]
-  let authorGroupMap=[]
-  
-    await ConceptCluster.findAll({attributes: ['name']}).then(response => {
-      console.log("first Concept Cluster is ",JSON.stringify(response[0]))
-      response.map(data => conceptClusterMap.push({name:data.name}));
-      
-
+async function generateConceptSitemap() {
+  let conceptMap = [];
+  await Concept.findAll({ attributes: ['name'] }).then(response => {
+    response.map(data => conceptMap.push({ name: data.name }));
+  })
+  const paramsConfig = {
+    "/Concepts/:name": conceptMap,
+  };
+  return (
+    new Sitemap(router)
+      .applyParams(paramsConfig)
+      .build(domain)
+      .save("./public/conceptsitemap.xml")
+  );
+}
+async function generateAuthorSiteMap() {
+  let authorMap = [];
+  await Author.findAll({ attributes: ['firstName', 'lastName'] }).then(response => {
+    response.map(data => authorMap.push({ name: data.firstName + "%20" + data.lastName }));
 
   })
-
-
-
-
-    let idMap = [{name:'love'},{name:'love'},{name:'educate'},{name:'cdk'},{name:'voice'},{name:'song'}];
-    // let authorMap = [{name:'Hao'},{name:'FriedRich'},{name:'Sigmund Frued'}];
-    // conceptMap=result.concepts;
-  
-
-    
-    
-    const paramsConfig = {
-        "/Concept-Clusters/:name":conceptClusterMap,
-      };
-  
-    
-    
-    return (
-        new Sitemap(router)
-            .applyParams(paramsConfig)
-            .build("http://conceptionary.io")
-            .save("./public/conceptclustersitemap.xml")
-      );
-  }
-  async function generateAuthorGroupSitedMap() {
-    let conceptMap=[];
-    let authorMap=[];
-    let conceptClusterMap=[]
-    let perspectiveMap=[]
-    let authorGroupMap=[]
-    
+const paramsConfig = {
+    "/Authors/:name": authorMap,
+  };
+  return (
+    new Sitemap(router)
+      .applyParams(paramsConfig)
+      .build(domain)
+      .save("./public/authorsitemap.xml")
+  );
+}
+async function generateConceptClusterSiteMap() {
+  let conceptClusterMap = []
+  await ConceptCluster.findAll({ attributes: ['name'] }).then(response => {
+    response.map(data => conceptClusterMap.push({ name: data.name }));
+  })
+  const paramsConfig = {
+    "/Concept-Clusters/:name": conceptClusterMap,
+  };
+  return (
+    new Sitemap(router)
+      .applyParams(paramsConfig)
+      .build(domain)
+      .save("./public/conceptclustersitemap.xml")
+  );
+}
+async function generateAuthorGroupSiteMap() {
+  let authorGroupMap = []
   await AuthorGroups.findAll({}).then(response => {
-    console.log("first Concept Cluster is ",JSON.stringify(response[0]))
-    response.map(data => authorGroupMap.push({name:data.name}));
-    
-  
+    response.map(data => authorGroupMap.push({ name: data.name }));
   })
-  
-  
-  
-      let idMap = [{name:'love'},{name:'love'},{name:'educate'},{name:'cdk'},{name:'voice'},{name:'song'}];
-      // let authorMap = [{name:'Hao'},{name:'FriedRich'},{name:'Sigmund Frued'}];
-      // conceptMap=result.concepts;
-    
-  
-      
-      
-      const paramsConfig = {
-          "/Author-Groups/:name":authorGroupMap,
-      
-        };
-    
-      
-      
-      return (
-          new Sitemap(router)
-              .applyParams(paramsConfig)
-              .build("http://conceptionary.io")
-              .save("./public/authorgroupsitemap.xml")
-        );
-    }
-  
-  generateSitemap();
-  generateAuthorSitedMap()
-  generateConceptClusterSitedMap()
-  // generateAuthorGroupSitedMap()
+  const paramsConfig = {
+    "/Author-Groups/:name": authorGroupMap,
+
+  };
+
+
+
+  return (
+    new Sitemap(router)
+      .applyParams(paramsConfig)
+      .build(domain)
+      .save("./public/authorgroupsitemap.xml")
+  );
+}
+async function generatePerspectiveSiteMap() {
+  let perspectiveMap = []
+  await Perspective.findAll({}).then(response => {
+    response.map(data => perspectiveMap.push({ id: data.id }));
+ })
+  let idMap = [{ id: 1 }, { id: 2 }];
+  const paramsConfig = {
+    "/perspective/:id": perspectiveMap,
+
+  };
+
+
+
+  return (
+    new Sitemap(router)
+      .applyParams(paramsConfig)
+      .build(domain)
+      .save("./public/perspectivesitemap.xml")
+  );
+}
+async function generateSitemap()
+{
+  await generateConceptSitemap();
+  await generateAuthorSiteMap()
+  await generateConceptClusterSiteMap()
+  await generateAuthorGroupSiteMap()
+  await generatePerspectiveSiteMap();
+  console.log(" SiteMap Generated Successfully !!!!")
+
+}
+
+
+generateSitemap()
--- sitemap-routes.js
+++ sitemap-routes.js
@@ -4,8 +4,8 @@ import { Route } from 'react-router';
 export default (
     <Route>
         <Route path='/' />
-        <Route path='/Concept/:name' />
-        <Route path='/Author/:name' />
+        <Route path='/Concepts/:name' />
+        <Route path='/Authors/:name' />
         <Route path='/Concept-Clusters/:name' />
         <Route path='/perspective/:id' />
         <Route path='/Author-Groups/:name' />
